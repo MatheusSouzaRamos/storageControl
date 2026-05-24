11dda95b155051e5935f4ba2b856f988873cfe1f
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-05-24 11:24:23 -0300

    adicionando produto service

---
+++ storage/src/main/java/com/projeto/storage/service/ProdutoService.java
@@ -0,0 +1,18 @@
+package com.projeto.storage.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.projeto.storage.model.Produto;
+import com.projeto.storage.repository.ProdutoRepository;
+
+public class ProdutoService {
+    
+    @Autowired
+    private ProdutoRepository repository;
+    
+    public List<Produto> findAll(){
+        return repository.findAll();
+    }
+}
