ec003b6188c8f7f5c54e4421cc113be71b7d77d6
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-05-25 16:19:02 -0300

    implementando: insert e update via js

--- storage/src/main/resources/static/script/produtos.js
+++ storage/src/main/resources/static/script/produtos.js
@@ -16,7 +16,13 @@ function buscarProdutos(){
 }
 
 function buscarProdutosId(){
-    let id = document.getElementById("ID").value;
+    let id = document.getElementById("idBuscar").value;
+
+    if(!id){
+        alert("Informe um id válido.")
+        return;
+    }
+
     fetch(`http://localhost:8080/produtos/${id}`, {
         method: "GET",
         headers: {
@@ -30,4 +36,81 @@ function buscarProdutosId(){
         console.log("Produto:", data);
     })
     .catch(erro => console.log("Erro: ", erro));
+}
+
+function inserirProduto(){
+    let nomeInsert = document.getElementById("NOMEINSERT").value;
+    let qntInsert = document.getElementById("QNTINSERT").value;
+    let valorInsert = document.getElementById("VALORINSERT").value;
+
+    console.log(nomeInsert);
+    console.log(qntInsert);
+    console.log(valorInsert);
+
+    if(!nomeInsert || nomeInsert.trim() == "" || !qntInsert || !valorInsert){
+        alert('Campos inválidos!');
+        return;
+    }
+
+    fetch("http://localhost:8080/produtos" , {
+        method: "POST",
+        headers: {
+            "Accept" : "application/json",
+            "Content-type": "application/json"
+        },
+
+        body: JSON.stringify({
+            nome: nomeInsert,
+            quantidade: qntInsert,
+            valor: valorInsert
+        })
+
+    }).then(res => {
+        if(!res.ok){
+            console.log("Erro ao inserir produto.")
+            return res.json()
+        }
+    })
+    .catch(erro => {
+        console.log("Erro: ", erro);
+    })
+}
+
+function updateProduto(){
+    let idupdate = document.getElementById("idupdate").value;
+    let nomeupdate = document.getElementById("nomeupdate").value;
+    let qntupdate = document.getElementById("qntupdate").value;
+    let valorupdate = document.getElementById("valorupdate").value;
+
+    console.log(idupdate)
+    console.log(nomeupdate);
+    console.log(qntupdate);
+    console.log(valorupdate);
+
+    if(!idupdate || !nomeupdate || nomeupdate.trim() == "" || !qntupdate || !valorupdate){
+        alert('Campos inválidos!');
+        return;
+    }
+
+    fetch(`http://localhost:8080/produtos/${idupdate}`, {
+        method: "PUT",
+        headers : {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nomeupdate,
+            quantidade: qntupdate,
+            valor: valorupdate
+        })
+    })
+    .then(res => {
+        if(!res.ok){
+            console.log("Erro ao atualizar produto.");
+            return;
+        }
+    })
+    .catch(erro => {
+        console.log("Erro: ", erro)
+    })
 }
